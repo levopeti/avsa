217b35598a13027c059f23644c4699b60817b57c
Eduardo <eduherreros0@gmail.com>
2022-03-15 11:53:53 +0100

    s

--- Lab_2/Lab2.0/Debug/objects.mk
+++ Lab_2/Lab2.0/Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS :=
+LIBS := -lopencv_core -lopencv_imgproc -lopencv_flann -lopencv_objdetect -lopencv_imgcodecs -lopencv_ml -lopencv_calib3d -lopencv_videoio -lopencv_features2d -lopencv_video -lopencv_highgui
 
